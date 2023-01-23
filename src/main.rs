3358f315aaa359e2a7c590ca1c1a6267fb9b540b
James Bull <contact@jamesbull.me>
2023-01-23 17:19:07 +1100

    hangs on error
    Last commit panics on permission denied.
    
    Current commit it an attempt to fix that. Currently hangs on error. I
    need to handle errors properly

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
+use std::env;
+use std::env::current_dir;
 use std::fs;
 
 fn main() {
-    let current_dir = std::env::current_dir().unwrap();
+    let args: Vec<String> = env::args().collect();
+    let current_dir = if args.len() > 1 {
+        std::path::PathBuf::from(&args[1])
+    } else {
+        current_dir().unwrap()
+    };
+    if !current_dir.is_dir() {
+        eprintln!("{} is not a directory", current_dir.to_str().unwrap());
+        return;
+    }
     find_git_repositories(current_dir);
 }
 
 fn find_git_repositories(dir: std::path::PathBuf) {
-    for entry in fs::read_dir(dir).unwrap() {
+    let entries = fs::read_dir(dir);
+    if entries.is_err() {
+        return;
+    }
+    let entries = entries.unwrap();
+    for entry in entries {
         let entry = entry.unwrap();
         let path = entry.path();
         if path.is_dir() {
