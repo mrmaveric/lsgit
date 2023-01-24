bb505164f2863588b4dec86a87247988d6e878b8
James Bull <contact@jamesbull.me>
2023-01-24 12:14:12 +1100

    More robust check for git folder
    I'm now checking for common git files in a folder to determine if it is
    a git folder instead of it's name.
    
    This should work for normal git folders containing a hidden .git folder
    as a sub folder.
    
    This should also work for bare git repositories regardless of their
    name (as long as the bare repo wasn't named "./.git" as that is the name
    used by a standard git subfolder).
    
    I've also moved the checking if a folder is named .git to it's own
    function because I didn't like how long it was.

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,11 @@ fn main() {
         return;
     }
 
-    if current_dir.join(".git").is_dir() {
+    if is_git_dir(&current_dir.join(".git")) {
+        println!("{}", current_dir.display());
+    }
+
+    if is_git_dir(&current_dir) && !is_named_git(&current_dir) {
         println!("{}", current_dir.display());
     }
 
@@ -34,10 +38,30 @@ fn find_git_repositories(dir: std::path::PathBuf) {
         let entry = entry.unwrap();
         let path = entry.path();
         if path.is_dir() {
-            if path.join(".git").is_dir() {
+            if is_git_dir(&path.join(".git")) {
+                println!("{}", path.display());
+            }
+            if is_git_dir(&path) && !is_named_git(&path) {
                 println!("{}", path.display());
             }
             find_git_repositories(path);
         }
     }
+}
+
+fn is_git_dir(path: &std::path::PathBuf) -> bool {
+    let git_files = ["HEAD", "config", "description", "hooks", "objects", "refs"];
+
+    for file in git_files.iter() {
+        let git_path = path.join(file);
+        if !git_path.exists() {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn is_named_git(path: &std::path::PathBuf) -> bool {
+    path.file_name().unwrap().to_str().unwrap() == ".git"
 }
